5a91f41945928a480c9af309316d6cf860cb085d
Spiros Vatikiotis <spirosfwiw@gmail.com>
2025-02-26 22:27:02 +0200

    add more movies

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,13 @@ const Card = ({ title }) => {
   const [count, setCount] = useState(0); 
   const [hasLiked, setHasLiked] = useState(false);
 
+
+
   useEffect( ()  => {
     console.log(`${title} has been Liked: ${hasLiked}`);
-  },
-    [hasLiked]);
-  
-  
-  
+  },[hasLiked]);
 
-  
+
   return (
     <div className="card" onClick={() => setCount(count + 1)}>
       <h2>{title} <br /> {count}</h2>
@@ -30,6 +28,9 @@ const App = () => {
       <Card title="Star Wars" rating={5} isCool={true} />
       <Card title="Avatar" />
       <Card title="The Lion King" />
+      <Card title="The Matrix" />
+      <Card title="The GodFather" />
+      <Card title="The Lord Of The Rings, The fellowship Of The Ring" />
     </div>
     
   
